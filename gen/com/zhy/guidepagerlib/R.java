b7a9fc9a2e67ff98f9262ddce13ee7f7248e524f
shyong <shihuiyongvip@126.com>
2016-03-23 23:16:15 +0800

    公测1.0.5 0323

--- gen/com/zhy/guidepagerlib/R.java
+++ gen/com/zhy/guidepagerlib/R.java
@@ -8,8 +8,8 @@ package com.zhy.guidepagerlib;
 
 public final class R {
 	public static final class dimen {
-		public static final int activity_horizontal_margin = 0x7f050000;
-		public static final int activity_vertical_margin = 0x7f050001;
+		public static final int activity_horizontal_margin = 0x7f070005;
+		public static final int activity_vertical_margin = 0x7f070006;
 	}
 	public static final class drawable {
 		public static final int ic_launcher = 0x7f020043;
@@ -30,12 +30,12 @@ public final class R {
 		public static final int main = 0x7f0b0009;
 	}
 	public static final class string {
-		public static final int action_settings = 0x7f060001;
-		public static final int app_name = 0x7f060000;
-		public static final int hello_world = 0x7f060002;
+		public static final int action_settings = 0x7f050008;
+		public static final int app_name = 0x7f050000;
+		public static final int hello_world = 0x7f050009;
 	}
 	public static final class style {
-		public static final int AppBaseTheme = 0x7f070000;
-		public static final int AppTheme = 0x7f070001;
+		public static final int AppBaseTheme = 0x7f060000;
+		public static final int AppTheme = 0x7f060001;
 	}
 }
